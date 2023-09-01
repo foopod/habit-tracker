585547074e18a0c2f1991edd1428f67e01fb4236
Jono Shields <jonathonshields@gmail.com>
2023-09-02 11:47:37 +1200

    update pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Activity Tracker",
+        short_name: "Activity Tracker",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#414bb2",
+        scope: "/",
+        description: "An app to track your habits and build a better you!",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icon-512.png",
+            size: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
