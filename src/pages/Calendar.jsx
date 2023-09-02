b7d7b1206667b7d196ac3a6baad5db9176ac6bd4
Jono Shields <jonathonshields@gmail.com>
2023-09-02 15:17:54 +1200

    Added calendar pagination

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -4,9 +4,28 @@ import { ActivityContext } from "../context/ActivityContext";
 import { Button } from "../components/Button";
 import "./Calendar.css";
 
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export const Calendar = () => {
   const { id } = useParams();
-  const currentMonth = new Date().getMonth();
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
+  const [hasNextMonth, setHasNextMonth] = useState(false);
+  const [hasPreviousMonth, setHasPreviousMonth] = useState(false);
   const { getActivity } = useContext(ActivityContext);
   const [activity, setActivity] = useState(null);
   const [currentData, setCurrentData] = useState([]);
@@ -18,13 +37,63 @@ export const Calendar = () => {
       if (requestedActivity) {
         setCurrentData(
           requestedActivity.data.filter((item) => {
-            return (new Date(item.date).getMonth = currentMonth);
+            return (
+              new Date(item.date).getMonth() === currentMonth &&
+              new Date(item.date).getFullYear() === currentYear
+            );
           })
         );
         setActivity(requestedActivity);
       }
     }
-  }, [id, getActivity]);
+  }, [id, getActivity, currentMonth, currentYear]);
+
+  useEffect(() => {
+    // check for previous and future data
+    if (activity) {
+      const next = hasDataForMonth(
+        activity.data,
+        incrementMonth(currentMonth, currentYear)
+      );
+      const previous = hasDataForMonth(
+        activity.data,
+        decrementMonth(currentMonth, currentYear)
+      );
+      setHasNextMonth(next);
+      setHasPreviousMonth(previous);
+    }
+  }, [activity, currentMonth, currentYear]);
+
+  const incrementMonth = (month, year) => {
+    if (month === 11) {
+      year += 1;
+      month = 0;
+    } else {
+      month += 1;
+    }
+    return { month, year };
+  };
+
+  const decrementMonth = (month, year) => {
+    if (month === 0) {
+      year -= 1;
+      month = 11;
+    } else {
+      month -= 1;
+    }
+    return { month, year };
+  };
+
+  const hasDataForMonth = (data, { month, year }) => {
+    return (
+      data.filter((item) => {
+        return (
+          new Date(item.date).getMonth() === month &&
+          new Date(item.date).getFullYear() === year
+        );
+      }).length > 0
+    );
+  };
 
   const renderDay = (day) => {
     const date = new Date(day.date);
@@ -55,6 +124,27 @@ export const Calendar = () => {
     <div className="container">
       <h1>Calendar</h1>
       <div>
+        <div className="calendar-pagination">
+          <Button
+            text={"<"}
+            disabled={!hasPreviousMonth}
+            onClick={() => {
+              const prev = decrementMonth(currentMonth, currentYear);
+              setCurrentMonth(prev.month);
+              setCurrentYear(prev.year);
+            }}
+          />
+          <h2>{months[currentMonth]}</h2>
+          <Button
+            text={">"}
+            disabled={!hasNextMonth}
+            onClick={() => {
+              const prev = incrementMonth(currentMonth, currentYear);
+              setCurrentMonth(prev.month);
+              setCurrentYear(prev.year);
+            }}
+          />
+        </div>
         {currentData &&
           currentData
             .sort((a, b) => new Date(a.date) > new Date(b.date))
@@ -62,6 +152,7 @@ export const Calendar = () => {
               return renderDay(item);
             })}
       </div>
+
       <div className="sticky-button">
         <Button
           onClick={() => {
